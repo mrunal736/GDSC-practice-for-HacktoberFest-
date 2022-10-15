55a50632e3409aa0c334ff0d95d408fc6d188cfc
gaurijoshi22 <99265590+gaurijoshi22@users.noreply.github.com>
2022-10-15 14:40:27 +0530

    Create Add.java
    Issue #1 Addition of two numbers in java.

---
+++ Java/Add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Add {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("For addition: \nEnter first number.");
+		double n1=sc.nextDouble();
+		System.out.println("Enter second number.");
+		double n2=sc.nextDouble();
+		double sum=n1+n2;
+		System.out.println("Addition= "+sum);
+	}
+}
