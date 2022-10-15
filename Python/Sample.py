95705a3d8101586776a82b05470ef158e1392f79
OjaswiPande <ojaswipande@gmail.com>
2022-10-15 15:42:40 +0530

    factorial program in python

--- Python/Sample.py
+++ Python/Sample.py
@@ -1,8 +1,20 @@
 print("this is a sample python program ")
-x = 1.10
-y = 1.0
-z = -35.59
+# Python program to find the factorial of a number provided by the user.
 
-print(type(x))
-print(type(y))
-print(type(z))
+# change the value for a different result
+
+
+# To take input from the user
+num = int(input("Enter a number: "))
+factorial=1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
