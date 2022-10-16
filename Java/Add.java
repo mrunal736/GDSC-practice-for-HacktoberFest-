d66eaaf856e3327eef05b7d9559fad8de711672d
ishabule <87441080+isha-73@users.noreply.github.com>
2022-10-16 22:54:33 +0530

    Add static sum method , which returns sum.

--- Java/Add.java
+++ Java/Add.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class Add {
+	static double sum(double n1,double n2)
+	{return n1+n2;}
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("For addition: \nEnter first number.");
 		double n1=sc.nextDouble();
 		System.out.println("Enter second number.");
 		double n2=sc.nextDouble();
-		double sum=n1+n2;
-		System.out.println("Addition= "+sum);
+		System.out.println("n1+n2 = "+sum(n1,n2));
 	}
 }
