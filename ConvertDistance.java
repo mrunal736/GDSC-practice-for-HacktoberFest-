0627164d92f43127e5e7671457460b66f0cbb32b
sayli2003 <95756364+sayli2003@users.noreply.github.com>
2022-10-17 16:21:31 +0530

    Add files via upload

---
+++ ConvertDistance.java
@@ -0,0 +1,15 @@
+package lispps;
+import java.util.Scanner;
+public class ConvertDistance {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		try {
+			int km=sc.nextInt();
+			System.out.format("The distance in Miles: %.3f", (km*0.621371));
+		}
+		catch (Exception e){
+			System.out.print("Enter an interger value:");
+		}
+		sc.close();
+	}
+}
